fbd35fe7d258a3eeec55919732cc9c5085f0d0c6
njabulomoyo <158990465+njabulomoyo@users.noreply.github.com>
2026-08-19 12:01:04 -0500

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    """
+    output: int - longest substring
+    edge cases: empty string? return 0, same chars, return 1
+
+    solution:
+    - initiate two pointers, slow and fast 
+    - move fast to find unique chars
+    - move slow if slow == fast
+
+    edge case, what if the prev char of fast is same as fast? 
+    - then we move slow to fast the start counting
+    - each time we move the pointers, we check the length
+    - return the length of the longest
+    """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        maxSub = 0 
+        subStr = set()
+         
+        slow, fast = 0, 0
+        while fast < len(s):
+            if s[fast] not in subStr:
+                subStr.add(s[fast])
+                maxSub = max(maxSub, fast - slow + 1)
+                fast += 1
+            else:
+                while s[fast] in subStr:
+                    subStr.remove(s[slow])
+                    slow += 1             
+        return maxSub
+                
+
+
+
+
+
+
+
+
+
+
+        
